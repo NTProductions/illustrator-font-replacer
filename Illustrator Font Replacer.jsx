e00ba0a353b411625dcda1b53bf5ab9745cb784c
Nate Lovell <Natelovell99@gmail.com>
2022-03-03 15:12:29 -0700

    Create Illustrator Font Replacer.jsx

---
+++ Illustrator Font Replacer.jsx
@@ -0,0 +1,24 @@
+﻿// illustrator font replacer
+
+main();
+
+function main() {
+    var doc = app.activeDocument;
+    
+    for(var l = 0; l < doc.layers.length; l++) {
+            if(doc.layers[l].textFrames.length > 0) {
+                for(var ll = 0; ll < doc.layers[l].textFrames.length; ll++) {
+                    textRange = doc.layers[l].textFrames[ll].textRange;
+                    characterAttributes  = textRange.characterAttributes;
+                    characterAttributes.textFont = randomFont();
+                    }
+                }
+        }
+    }
+
+function randomFont() {
+        var fonts = app.textFonts;
+       
+        var randInt = Math.floor(Math.random() * fonts.length-1);
+        return fonts[randInt];
+    }
